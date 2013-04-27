7e0c1ea458eff5c53c21d881824eaf9148606ec3
ducvan0212 <ducvan0212@gmail.com>
2013-04-27 18:34:21 +0700

    Resume update feature

--- src/Acme/WorkBundle/Controller/ResumeController.php
+++ src/Acme/WorkBundle/Controller/ResumeController.php
@@ -110,4 +110,69 @@ class ResumeController extends Controller
             'resume' => $resume,
             ));
     }
+
+    public function updateAction($id)
+    {
+      if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+        throw new AccessDeniedException();
+      }
+
+      $em = $this->getDoctrine()->getManager();
+      $resume = $em->getRepository('AcmeWorkBundle:Resume')->find($id);
+      if (!$resume) {
+        throw $this->createNotFoundException( 
+          'No resume found'
+        );
+      }
+
+      $request = $this->get('request');
+
+      $form = $this->createFormBuilder($resume)
+        ->add('languages', 'entity', array(
+        'class' => 'AcmeWorkBundle:Language',
+        'property' => 'name',
+        'expanded' => true,
+        'multiple' => true))
+        ->add('categories', 'entity', array(
+        'class' => 'AcmeWorkBundle:Category',
+        'property' => 'name',
+        'expanded' => true,
+        'multiple' => true))
+        ->add('salary', 'entity', array(
+        'class' => 'AcmeWorkBundle:Salary',
+        'property' => 'level',
+        'expanded' => false,
+        'multiple' => false))
+        ->add('jobPosition', 'entity', array(
+        'class' => 'AcmeWorkBundle:JobPosition',
+        'property' => 'name',
+        'expanded' => false,
+        'multiple' => false))
+        ->add('location', 'entity', array(
+        'class' => 'AcmeWorkBundle:Location',
+        'property' => 'name',
+        'expanded' => false,
+        'multiple' => false))
+      ->getForm();
+      if ($request->isMethod('POST')) {
+        $form->bind($request);
+        
+        if ($form->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($resume);
+          $em->flush();
+          return new Response('okok'); 
+        } else {
+          return $this->render('AcmeWorkBundle:Resume:edit.html.twig', array(
+            'form' => $form->createView(),
+            'resume' => $resume,
+            ));
+        }
+      } else {
+        return $this->render('AcmeWorkBundle:Resume:edit.html.twig', array(
+            'form' => $form->createView(),
+            'resume' => $resume,
+            ));
+      }      
+    }
 }
